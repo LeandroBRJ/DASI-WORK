f8f994f4893d3670e82fafefaee015a751a341e4
Leandro DA SILVA <leandrobrj25@gmail.com>
2018-10-20 10:54:00 +0200

    Maj des fichiers au long de la séance

--- src/Mvc/Request/Request.php
+++ src/Mvc/Request/Request.php
@@ -7,13 +7,15 @@
 	class Request{
 
 		//declarer une constante
-		const NIVEAU_AMATEUR = 1;
+		const HTTP_GET  = 'GET';
+		const HTTP_POST = 'POST';
 
 		//les proprietes
 		protected $_controller;  //protected, pour empecher son acces à quelqu’un non autorisé
 		protected $_action;       //en PHP : protected permet juste aux membres de classe d’acceder à l’attribut
 		protected $_url;
 		protected $_baseUrl;
+		protected $_aHttpParams; //c'est lui qui va contenir les parametres de get ou post
 
 		
 		//constructeur de la classe
@@ -23,54 +25,66 @@
 			if(! isset($_SERVEUR['REQUEST_URI'])){ // si REQUEST_URI n'est pas renseigné, on déclenche une exception
 				throw new Exception("Le request n'est pas renseigné", 1);				
 			}	
-			$this -> _url = $_SERVEUR['REQUEST_URI'];
+			$this->_url = $_SERVEUR['REQUEST_URI'];
+
+			//HTTP_GET pour recevoir $_GET['...'], HTTP_POST pour recevoir $_POST['...']			
+			$this->_aHttpParams[self::HTTP_GET] = $_GET[''];
+			$this->_aHttpParams[self::HTTP_POST] = $_POST[''];			
+
 		}		
 		
 		
 		//methode accesseur (getter)
 		public function getController(){
-			return $this -> _controller ;
+			return $this->_controller ;
 		}
 
 		//methode accesseur (getter)
 		public function getAction(){
-			return $this -> _action ;
+			return $this->_action ;
 		}
 
 		//methode accesseur (getter)
 		public function getUrl(){
-			return $this -> _url;
+			return $this->_url;
 		}
 
 		//methode accesseur (getter)
 		public function getBaseUrl(){
-			return $this -> _baseUrl;
+			return $this->_baseUrl;
 		}
 
 		//methode accesseur (getter)
 		public function setController($ctrl){
-			$this -> _controller = $ctrl ;
+			$this->_controller = $ctrl ;
 			return $this ;	
 		}
 
 		//methode accesseur (getter)
 		public function setAction($ctrl){
-			$this -> _action = $ctrl ;
+			$this->_action = $ctrl ;
 			return $this ;	
 		}		
 
 		//methode accesseur (getter)
 		public function setUrl($ctrl){
-			$this -> _url = $ctrl ;
+			$this->_url = $ctrl ;
 			return $this ;	
 		}		
 
 		//methode accesseur (getter)
 		public function setBaseUrl($ctrl){
-			$this -> _baseUrl = $ctrl ;
+			$this->_baseUrl = $ctrl ;
 			return $this ;	
 		}		
 
+		public  function isPost(){
+			if($_SERVEUR['REQUEST_METHOD'] === "POST" ){
+				return true;
+			}else{
+				return false;
+			}
+		}
 
 		/*
 		public  function marcher(){
